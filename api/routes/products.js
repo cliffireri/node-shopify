1707a4a73a5400dce452fc169e5c22309252192d
cliffireri <irericlifton@yahoo.com>
2020-04-28 22:44:12 +0300

    add logic to routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,9 +4,17 @@ const Product = require('../models/product')
 const mongoose = require('mongoose')
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message:'Serving get products'
-    })
+   Product.find().exec().then((products => {
+       console.log(products)
+       res.status(200).json({
+           products: products
+       })
+   })).catch(error => {
+       console.log(error)
+       res.status(500).json({
+           error: error
+       })
+   })
 })
 router.post('/', (req, res, next) => {
     const product = new Product({
@@ -26,7 +34,14 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req,res,next) => {
     const id = req.params.productId;
     Product.findById(id).exec().then(response => {
-        res.status(200).json(response)
+        if(response){
+            res.status(200).json(response)
+        }else{
+            res.status(404).json({
+                message: "No product with that id was found"
+            })
+        }
+        
     }).catch(error => {
         console.log(error)
         res.status(500).json({error: error})
@@ -34,14 +49,31 @@ router.get('/:productId', (req,res,next) => {
 })
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message:'Updated products'
+    const id = req.params.productId
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName]=ops.value
+    }
+    Product.update({_id:id}, {$set: updateOps }).exec().then(result => {
+        console.log(result)
+        res.status(200).json(result)
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        })
     })
 })
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message:'Deleted products'
+    const id = req.params.productId;
+    Product.remove({_id:id}).exec().then(result => {
+        console.log(result)
+        res.status(200).json(result)
+    }).catch(error => {
+        console.log(500)
+        res.status(500).json({
+            error: error
+        })
     })
 })
 
