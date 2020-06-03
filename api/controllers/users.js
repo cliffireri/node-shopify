46299fffb129c6b82a8dc405407c56d862dd4371
cliffireri <irericlifton@yahoo.com>
2020-06-03 21:35:13 +0300

    adding controller for user logic

---
+++ api/controllers/users.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+
+const User = require('../models/user')
+
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
+exports.user_signup = (req, res, next) => {
+    User.find({email: req.body.email}).exec().then(user => {
+        if(user.length >= 1){
+            return res.status(409).json({
+                error: "Email already exists"
+            })
+        }else{
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if(err){
+                    res.status(500).json({
+                        error: err
+                    })
+                }else{
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: hash
+                    })
+                    user.save().then(result => {
+                        res.status(200).json({
+                            message: 'User created'
+                        })
+                    }).catch(error => {
+                        res.status(500).json({
+                            error: error
+                        })
+                    })
+                }
+            })
+        }
+    })
+}
+
+exports.user_login = (req, res, next) => {
+    User.find({email: req.body.email}).exec().then(user => {
+        if(user.length < 1){
+            return res.status(401).json({message: "Auth failed"})
+        }
+
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if(err){
+                return res.status(401).json({message: "Auth failed"})
+            }
+            if(result){
+                const token = jwt.sign({
+                    email: user[0].email,
+                    id:user[0]._id
+                },process.env.JWT_KEY,{
+                    expiresIn:"1h"
+                });
+                return res.status(200).json({
+                    message: 'Auth successful',
+                    token: token
+                })
+            }
+            res.status(401).json({
+                message: "Auth failed",
+            })
+        })
+
+    }).catch(error => {
+        res.status(500).json({error: error})
+    })
+}
+
+exports.user_delete = (req, res, next) => {
+    const uid = req.params.userId
+    User.remove({_id:uid}).exec().then((result) => {
+        res.status(200).json({
+            message: "Deleted successfully"
+        })
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        })
+    })
+}
+
+exports.get_users_all = (req, res, next) => {
+    User.find().select('_id email').exec().then(result => {
+        res.status(200).json({users: result});
+    }).catch(error => {
+        res.status(500).json({error: error})
+    })
+}
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,99 +2,15 @@ const express = require('express')
 
 const router = express.Router();
 
-const mongoose = require('mongoose');
 
-const User = require('../models/user')
+const UsersController = require('../controllers/users')
 
-const bcrypt = require('bcryptjs');
+router.post('/signup', UsersController.user_signup)
 
-const jwt = require('jsonwebtoken');
+router.post('/login', UsersController.user_login)
 
-router.post('/signup', (req, res, next) => {
-    User.find({email: req.body.email}).exec().then(user => {
-        if(user.length >= 1){
-            return res.status(409).json({
-                error: "Email already exists"
-            })
-        }else{
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if(err){
-                    res.status(500).json({
-                        error: err
-                    })
-                }else{
-                    const user = new User({
-                        _id: new mongoose.Types.ObjectId(),
-                        username: req.body.username,
-                        email: req.body.email,
-                        password: hash
-                    })
-                    user.save().then(result => {
-                        res.status(200).json({
-                            message: 'User created'
-                        })
-                    }).catch(error => {
-                        res.status(500).json({
-                            error: error
-                        })
-                    })
-                }
-            })
-        }
-    })
-})
+router.delete('/:userId', UsersController.user_delete)
 
-router.post('/login', (req, res, next) => {
-    User.find({email: req.body.email}).exec().then(user => {
-        if(user.length < 1){
-            return res.status(401).json({message: "Auth failed"})
-        }
-
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-            if(err){
-                return res.status(401).json({message: "Auth failed"})
-            }
-            if(result){
-                const token = jwt.sign({
-                    email: user[0].email,
-                    id:user[0]._id
-                },process.env.JWT_KEY,{
-                    expiresIn:"1h"
-                });
-                return res.status(200).json({
-                    message: 'Auth successful',
-                    token: token
-                })
-            }
-            res.status(401).json({
-                message: "Auth failed",
-            })
-        })
-
-    }).catch(error => {
-        res.status(500).json({error: error})
-    })
-})
-
-router.delete('/:userId', (req, res, next) => {
-    const uid = req.params.userId
-    User.remove({_id:uid}).exec().then((result) => {
-        res.status(200).json({
-            message: "Deleted successfully"
-        })
-    }).catch(error => {
-        res.status(500).json({
-            error: error
-        })
-    })
-})
-
-router.get('/', (req, res, next) => {
-    User.find().select('_id email').exec().then(result => {
-        res.status(200).json({users: result});
-    }).catch(error => {
-        res.status(500).json({error: error})
-    })
-})
+router.get('/', UsersController.get_users_all)
 
 module.exports = router;
